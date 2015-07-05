27f7971e109ee624a5fd3036a05a69e3c02ced14
mashazavolnyuk <mashazavolnyuk@yandex.ru>
2015-07-05 12:09:16 +0300

    1235:
    added class BD.cs(show all commit from BD)

---
+++ HomeTask/BD.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeTask
+{
+    class BD
+    {
+        private string name;
+        private List <Commit> my commit;
+
+        public BD(string Name){
+            this.name=Name;
+        }
+
+    }
+}
